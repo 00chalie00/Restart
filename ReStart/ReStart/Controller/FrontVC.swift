e19333e7c926da15adf6664c2d18afd13de1626b
Lee Chul <winfe79@hanmail.net>
2019-03-15 17:18:27 +0900

    uiUpInit

---
+++ ReStart/ReStart/Controller/FrontVC.swift
@@ -0,0 +1,21 @@
+//
+//  FrontVC.swift
+//  ReStart
+//
+//  Created by formathead on 15/03/2019.
+//  Copyright © 2019 formathead. All rights reserved.
+//
+
+import UIKit
+
+class FrontVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}// End Of The Class
+
---
+++ ReStart/ReStart/Controller/RearVC.swift
@@ -0,0 +1,19 @@
+//
+//  RearVC.swift
+//  ReStart
+//
+//  Created by formathead on 15/03/2019.
+//  Copyright © 2019 formathead. All rights reserved.
+//
+
+import UIKit
+
+class RearVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}//End Of The Class
+
